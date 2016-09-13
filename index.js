2beeb45852a8bb9619556da0e53163f7ddd72189
Qi He <qhe@heyook.com>
2016-09-12 17:52:13 -0700

    added redis url

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const S3Notifier = require('fastboot-s3-notifier');
 const REDIS_HOST   = process.env.FASTBOOT_REDIS_HOST;
 const REDIS_PORT   = process.env.FASTBOOT_REDIS_PORT;
 const REDIS_EXPIRY = process.env.FASTBOOT_REDIS_EXPIRY;
+const REDIS_URL    = process.env.FASTBOOT_REDIS_URL;
 
 const S3_PARAMS = {
   bucket: process.env.FASTBOOT_S3_BUCKET,
@@ -18,14 +19,15 @@ const downloader = new S3Downloader(S3_PARAMS);
 const notifier = new S3Notifier(S3_PARAMS);
 
 let cache;
-if (REDIS_HOST || REDIS_PORT) {
+if (REDIS_HOST || REDIS_PORT || REDIS_URL) {
   cache = new RedisCache({
     host: REDIS_HOST,
     port: REDIS_PORT,
+    url: REDIS_URL,
     expiration: REDIS_EXPIRY
   });
 } else {
-  console.log('No FASTBOOT_REDIS_HOST or FASTBOOT_REDIS_PORT provided; caching is disabled.');
+  console.log('No FASTBOOT_REDIS_HOST or FASTBOOT_REDIS_PORT or FASTBOOT_REDIS_URL provided; caching is disabled.');
 }
 
 const server = new FastBootAppServer({
